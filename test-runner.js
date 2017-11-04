ab0c31e71fd3b9ababece5036b6650ab14f2b825
David Walters <whatbirdisthat@gmail.com>
2017-11-04 11:29:41 +1100

    small refactor to test runner to make it even more awesome

--- test-runner.js
+++ test-runner.js
@@ -1,19 +1,28 @@
 module.exports = {
 
   RunTests: function(theTests, setupFunc) {
-    for (t in theTests) {
-      let Reset = "\x1b[0m"
-      let FgGreen = "\x1b[32m"
-      let FgRed = "\x1b[31m"
 
-      let thisResult = FgGreen + "\u2713 " + Reset + t
+    let Reset = "\x1b[0m"
+    let FgGreen = "\x1b[32m"
+    let FgRed = "\x1b[31m"
+
+    let TickMark = "\u2713"
+    let CrossMark = "\u2717"
+
+    for (t in theTests) {
+
+      let thisResult = FgGreen + TickMark + " " + Reset + t
       try {
+
         let ObjectUnderTest = setupFunc()
         theTests[t](ObjectUnderTest)
+
       } catch (e) {
-        thisResult = FgRed + "\u2717 " + Reset + t + ' (' + e + ')'
+        thisResult = FgRed + CrossMark + " " + Reset + t + ' (' + e + ')'
       }
+
       console.log(thisResult)
+
     }
 
   }
